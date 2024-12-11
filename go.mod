7c90c339c05017df96470da9c572cb1542b284a9
charlesvt <cvut1102@gmail.com>
2024-12-11 04:24:06 -0800

    finalized shred function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module shred-test
+
+go 1.23.4
--- main.go
+++ main.go
@@ -1,82 +1,118 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "crypto/rand"
+	"crypto/rand"
+	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-   //Path to file
-   filePath := "randomfile.txt"
-   
-   err := createRandFile()
-   
-   err := randomDataGen(filePath)
-   //Error handler for randomDataGen function
-   if err != nil {
-       log.Fatalf("Failed to randomize/overwrite file: %v", err)
-   } else {
-   	fmt.Println("File overwritten successfully")
-   }
-   
-   err = shredOnly(filePath)
-   //Error handler for shred function
-   if err != nil {
-       log.Fatalf("Failed to shred file: %v", err)
-   } else {
-   	fmt.Println("File shredded successfully")
-   }
+	//Path to file
+	filePath := "randomfile.txt"
+
+	err := Shred(filePath)
+	if err != nil {
+		log.Fatalf("File failed to shred: %v", err)
+	} else {
+		fmt.Println("File shredded successfully")
+	}
+
+	// err := createRandFile(filePath)
+	// if err != nil {
+	// 	log.Fatalf("Failed to create file: %v", err)
+	// } else {
+	// 	fmt.Println("File created successfully")
+	// }
+
+	// err = randomDataGen(filePath)
+	// //Error handler for randomDataGen function
+	// if err != nil {
+	// 	log.Fatalf("Failed to randomize/overwrite file: %v", err)
+	// } else {
+	// 	fmt.Println("File overwritten successfully")
+	// }
+
+	// err = shredOnly(filePath)
+	// //Error handler for shred function
+	// if err != nil {
+	// 	log.Fatalf("Failed to shred file: %v", err)
+	// } else {
+	// 	fmt.Println("File shredded successfully")
+	// }
+}
+
+func Shred(path string) error {
+	// Checking to see if file exists
+	file, err := os.OpenFile(path, os.O_WRONLY, 0) // For write access.
+	if err != nil {
+		return err
+	}
+	defer file.Close() // Ensures file deletion even in case of error
+
+	file.Close()
+	return os.Remove(path)
 }
 
 func shredOnly(path string) error {
-    // Checking to see if file exists
-    file, err := os.OpenFile(path, os.O_WRONLY, 0) // For write access.
-    if err != nil {
-        return err
-    }
-    defer file.Close() // Ensures file deletion even in case of error
-    
-    file.Close()
-    return os.Remove(path)
+	// Checking to see if file exists
+	file, err := os.OpenFile(path, os.O_WRONLY, 0) // For write access.
+	if err != nil {
+		return err
+	}
+	defer file.Close() // Ensures file deletion even in case of error
+
+	// Randomize data 3x
+	for i := 0; i < 3; i++ {
+		randomData := make([]byte, 1024)
+		if _, err := rand.Read(randomData); err != nil {
+			return err
+		}
+		if _, err := file.WriteAt(randomData, 0); err != nil {
+			return err
+		}
+	}
+
+	file.Close()
+	return os.Remove(path)
 }
 
 func randomDataGen(path string) error {
-    // Checking to see if file exists
-    file, err := os.OpenFile(path, os.O_WRONLY, 0) // For write access.
-    if err != nil {
-        return err
-    }
-    defer file.Close() // Ensures file deleltion even in case of error
-    
-    // Randomize data 3x
-    for i := 0; i < 3; i++ {
-        randomData := make([]byte,1024)
-        if _, err := rand.Read(randomData); err != nil {
-            return err
-        }
-        if _, err := file.WriteAt(randomData, 0); err != nil {
-            return err
-        }
-    }
-    
-    return file.Close()
+	// Checking to see if file exists
+	file, err := os.OpenFile(path, os.O_WRONLY, 0) // For write access.
+	if err != nil {
+		return err
+	}
+	defer file.Close() // Ensures file deleltion even in case of error
+
+	// Randomize data 3x
+	for i := 0; i < 3; i++ {
+		randomData := make([]byte, 1024)
+		if _, err := rand.Read(randomData); err != nil {
+			return err
+		}
+		if _, err := file.WriteAt(randomData, 0); err != nil {
+			return err
+		}
+	}
+
+	return file.Close()
 }
 
 func createRandFile(path string) error {
-    // Checking to see if file exists
-    file, err := os.OpenFile(path, os.O_WRONLY, 0) // For write access.
-    if err = nil {
-        return err
-    }
-    defer file.Close() // Ensures file deleltion even in case of error
-    
-    content := []byte("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
-    
-    _, err := os.WriteFile(path, content, 0644)
-    if err = nil {
-        return err
-    }
-       
+	// Checking to see if file exists
+	file, err := os.OpenFile(path, os.O_WRONLY, 0) // For write access.
+	if err != nil {
+		return err
+	}
+	defer file.Close() // Ensures file deleltion even in case of error
+
+	content := []byte("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+
+	err = os.WriteFile(path, content, 0644)
+	if err != nil {
+		return err
+	}
+	file.Close()
+	return err
 }
